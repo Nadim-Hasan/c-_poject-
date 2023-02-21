0a565cd27605a425441f533215faf896c24f8e00
MD. NADIM HASAN <68935090+Nadim-Hasan@users.noreply.github.com>
2023-02-21 10:50:42 +0600

    Add files via upload
    This is 2 layer architecture project on a "Super Shop Management Desktop Application". one is Operation and another is the Entities section. It can be upgraded using advanced framework.

---
+++ Market_Management/SignUpForm.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DataAccessLayer_Market.Operations;
+using DataAccessLayer_Market.Entities;
+using System.Data.SqlClient;
+
+namespace Market_Management
+{
+    public partial class SignUpForm : Form
+    {
+        public SignUpForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            EMForm1 EMF = new EMForm1();
+            OMForm1 OMF = new OMForm1();
+
+            EMF.UserName = Cnametxt.Text;
+            EMF.UserPhone= CPhonetxt.Text;
+            EMF.UserPass = Cpasstxt.Text;
+            EMF.UserCategory = ccPasstxt.Text;
+            // pro.ProdCategory = ProCatCB.SelectedItem.ToString();
+            //pro.ProdCategory = prodCategoryCBX.SelectedText.ToString();
+            if (Cpasstxt.Text != ccPasstxt.Text) 
+            {
+                MessageBox.Show("Please check the password again");
+                    }
+            else
+            {
+                int flag = OMF.Signup(EMF);
+                //SqlConnection Con = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\user\Documents\MarketDATABASE.mdf;Integrated Security=True;Connect Timeout=30");
+                if (flag > 0)
+                {
+                    MessageBox.Show("Sucessfully inserted");
+                }
+                else
+                {
+                    MessageBox.Show("Not Inserted ");
+                }
+            }
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Form1 login = new Form1();
+            login.Show();
+            this.Hide();
+
+        }
+    }
+}
